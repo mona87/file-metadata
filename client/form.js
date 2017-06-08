f75f9e5477944abc9ab8ae1bbd349f67d8142f29
mona87 <bellamy2727@yahoo.com>
2017-06-08 11:20:53 -0500

    refactor

--- client/form.js
+++ client/form.js
@@ -1,42 +1,42 @@
+//code from client
+// let callback = function() {
+//   let file = [];
+//   const submit = document.querySelector('#sub');
+//   //get file
+//   document.querySelector('#fileinput').addEventListener('change', function() {
+//     console.log(this.files[0]);
+//     file = this.files[0];
+//   });
 
-let callback = function() {
-  let file = [];
-  const submit = document.querySelector('#sub');
-  //get file
-  document.querySelector('#fileinput').addEventListener('change', function() {
-    console.log(this.files[0]);
-    file = this.files[0];
-  });
 
+//   submit.addEventListener('click', (e) => {
+//     e.preventDefault();
 
-  submit.addEventListener('click', (e) => {
-    e.preventDefault();
+//     //create forData object
+//     let formData = new FormData();
+//     formData.append('file', file);
 
-    //create forData object
-    let formData = new FormData();
-    formData.append('file', file);
+//     const form = document.querySelector('#form');
+//     console.log(file, 'f')
+//     fetch("/post", {
+//         method: "POST",
+//         body: formData,
+//       })
+//       .then(function(result) {
+//         if (result.status === 200) {
+//           //redirect to json
+//           window.location = result.url;
+//         }
+//       }).catch(err => {
+//         console.log(err);
+//       });
+//   })
+// }
 
-    const form = document.querySelector('#form');
-    console.log(file, 'f')
-    fetch("/post", {
-        method: "POST",
-        body: formData,
-      })
-      .then(function(result) {
-        if (result.status === 200) {
-          //redirect to json
-          window.location = result.url;
-        }
-      }).catch(err => {
-        console.log(err);
-      });
-  })
-}
-
-//check DOM is ready
-if (document.readyState === "complete" || (document.readyState !== "loading" && !document.documentElement.doScroll)) {
-  callback();
-} else {
-  document.addEventListener("DOMContentLoaded", callback);
-}
+// //check DOM is ready
+// if (document.readyState === "complete" || (document.readyState !== "loading" && !document.documentElement.doScroll)) {
+//   callback();
+// } else {
+//   document.addEventListener("DOMContentLoaded", callback);
+// }
 
--- server.js
+++ server.js
@@ -8,14 +8,8 @@ app.use(formidable());
 
 app.post('/post', (req, res) =>{
 
-	const size = req.files.file.size;
-	res.redirect('/get-file-size/' + size);
+	res.json({size: parseInt(req.files.file.size)});
 });
-
-app.get('/get-file-size/:param', (req, res) => {
-	res.json({size: parseInt(req.params.param)});
-});
-
 
 app.listen(process.env.PORT || 3000, process.env.IP || '0.0.0.0', () => {
 	console.log('app is listening on port 3000')
